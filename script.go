0cb6c7f725d59a8e14a466b7d5234805fce5966d
mike andrews <mra@xoba.com>
2026-01-13 13:25:08 -0500

    send 'text' after 'file'

--- script.go
+++ script.go
@@ -18,17 +18,17 @@ func buildSendScript(recipient, text string, attachments []attachmentInfo) strin
 	b.WriteString("    set targetService to 1st service whose service type is iMessage and enabled is true\n")
 	b.WriteString(fmt.Sprintf("    set targetBuddy to buddy \"%s\" of targetService\n", escapedRecipient))
 
-	if text != "" {
-		escapedText := escapeAppleScriptString(text)
-		b.WriteString(fmt.Sprintf("    send \"%s\" to targetBuddy\n", escapedText))
-	}
-
 	for _, attachment := range attachments {
 		escapedPath := escapeAppleScriptString(attachment.Path)
 		b.WriteString(fmt.Sprintf("    send POSIX file \"%s\" to targetBuddy\n", escapedPath))
 		if attachment.DelaySecond > 0 {
 			b.WriteString(fmt.Sprintf("    delay %d\n", attachment.DelaySecond))
 		}
+	}
+
+	if text != "" {
+		escapedText := escapeAppleScriptString(text)
+		b.WriteString(fmt.Sprintf("    send \"%s\" to targetBuddy\n", escapedText))
 	}
 
 	b.WriteString("end tell")
